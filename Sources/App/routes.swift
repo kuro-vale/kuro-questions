853b1444e01f502276850ea36161c19267852741
kuro-vale <jsalcedo218@gmail.com>
2022-10-12 20:37:10 +0000

    Initial commit

---
+++ Sources/App/routes.swift
@@ -0,0 +1,14 @@
+import Fluent
+import Vapor
+
+func routes(_ app: Application) throws {
+  app.get { req async in
+    "It works!"
+  }
+
+  app.get("health") { req async -> String in
+    "Ok"
+  }
+
+  // try app.register(collection: TodoController())
+}
