ab86d986a2f33ceb4fd6b04fe5dfd29b1d538d8d
kuro-vale <jsalcedo218@gmail.com>
2022-10-21 18:45:53 -0500

    Add tests

--- Tests/AppTests/Controllers/QuestionControllerTest.swift
+++ Tests/AppTests/Controllers/QuestionControllerTest.swift
@@ -11,30 +11,126 @@ final class QuestionControllerTest: XCTestCase {
 
   // GET /questions
   func testIndex() throws {
-    let question = try newQuestion(on: app.db)
+    let user = try newUser(on: app.db)
+    let question = try newQuestion(on: app.db, user: user)
 
     try app.test(
       .GET, "questions",
       afterResponse: { res in
         XCTAssertEqual(res.status, .ok)
-        let questions = try res.content.decode(PaginatedQuestions.self)
-        XCTAssertEqual(questions.items[0].body, question.body)
+        let response = try res.content.decode(PaginatedQuestions.self)
+        XCTAssertEqual(response.items[0].body, question.body)
       })
   }
 
   // GET /questions/search
   func testSearch() throws {
-    let question = try newQuestion(category: Category.arts, on: app.db)
-    let _ = try newQuestion(on: app.db)
+    let user = try newUser(on: app.db)
+    let question = try newQuestion(category: Category.arts, on: app.db, user: user)
+    let _ = try newQuestion(on: app.db, user: user)
 
     try app.test(
       .GET, "questions/search?category=Arts",
       afterResponse: { res in
         XCTAssertEqual(res.status, .ok)
-        let questions = try res.content.decode(PaginatedQuestions.self)
-        XCTAssertEqual(questions.items.count, 1)
-        XCTAssertEqual(questions.items[0].body, question.body)
+        let response = try res.content.decode(PaginatedQuestions.self)
+        XCTAssertEqual(response.items.count, 1)
+        XCTAssertEqual(response.items[0].body, question.body)
       })
+  }
+
+  // GET /questions/:id
+  func testGetOne() throws {
+    let user = try newUser(on: app.db)
+    let question = try newQuestion(category: Category.arts, on: app.db, user: user)
+    try app.test(
+      .GET, "questions/\(question.id!)",
+      afterResponse: { res in
+        XCTAssertEqual(res.status, .ok)
+        let response = try res.content.decode(QuestionResponse.self)
+        XCTAssertEqual(response.body, question.body)
+      })
+  }
+
+  // GET /questions/me
+  func testUserQuestions() throws {
+    // Verify tests does not get other users questions
+    let anotherUser = try newUser(on: app.db)
+    let _ = try newQuestion(category: Category.arts, on: app.db, user: anotherUser)
+    // Generate token
+    let user = try newUser("testUserQuestions", "bar123",on: app.db)
+    let question = try newQuestion(category: Category.arts, on: app.db, user: user)
+    var token: String = ""
+    try app.test(
+      .POST, "auth/login",
+      beforeRequest: { req in
+        try req.content.encode(UserRequest(username: user.username, password: "bar123"))
+      },
+      afterResponse: { res in
+        let response = try res.content.decode(UserResponse.self)
+        token = response.token
+      }
+    )
+    // 200 if using a valid token
+    try app.test(
+      .GET, "questions/me",
+      beforeRequest: { req in
+        req.headers.bearerAuthorization = .init(token: token)
+      },
+      afterResponse: { res in
+        XCTAssertEqual(res.status, .ok)
+        let response = try res.content.decode(PaginatedQuestions.self)
+        XCTAssertEqual(response.items.count, 1)
+        XCTAssertEqual(response.items[0].body, question.body)
+      }
+    )
+    // 401 if not token present
+    try app.test(
+      .GET, "questions/me",
+      afterResponse: { res in
+        XCTAssertEqual(res.status, .unauthorized)
+      }
+    )
+  }
+
+  // POST /questions
+  func testCreate() throws {
+    let content = ["body": "What is love?", "category": "General Knowledge"]
+    // Generate token
+    var token: String = ""
+    try app.test(
+      .POST, "auth/register",
+      beforeRequest: { req in
+        try req.content.encode(UserRequest(username: "testCreate", password: "bar123"))
+      },
+      afterResponse: { res in
+        let response = try res.content.decode(UserResponse.self)
+        token = response.token
+      }
+    )
+    // 200 if using a valid token
+    try app.test(
+      .POST, "questions",
+      beforeRequest: { req in
+        req.headers.bearerAuthorization = .init(token: token)
+        try req.content.encode(content)
+      },
+      afterResponse: { res in
+        XCTAssertEqual(res.status, .ok)
+        let response = try res.content.decode(QuestionResponse.self)
+        XCTAssertEqual(response.body, content["body"])
+      }
+    )
+    // 401 if not token present
+    try app.test(
+      .POST, "questions",
+      beforeRequest: { req in
+        try req.content.encode(content)
+      },
+      afterResponse: { res in
+        XCTAssertEqual(res.status, .unauthorized)
+      }
+    )
   }
 
   override func tearDownWithError() throws {
--- Tests/AppTests/Fixtures/QuestionFixtures.swift
+++ Tests/AppTests/Fixtures/QuestionFixtures.swift
@@ -3,9 +3,9 @@ import Fluent
 @testable import App
 
 func newQuestion(
-  body: String = "Lorem Ipsum", category: Category = Category.technology, on database: Database
+  body: String = "Lorem Ipsum", category: Category = Category.technology, on database: Database, user: User
 ) throws -> Question {
-  let question = Question(body, category)
+  let question = Question(body, category, user.id!)
   try question.save(on: database).wait()
   return question
 }
