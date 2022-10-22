a0d6dddd5241fde9e567cc217ddb263c0a85b76b
kuro-vale <jsalcedo218@gmail.com>
2022-10-22 18:55:14 -0500

    Add missing ampersand in paginator

--- Sources/App/Helpers/Paginator.swift
+++ Sources/App/Helpers/Paginator.swift
@@ -15,14 +15,18 @@ func serverMetadataAssembler(_ metadata: PageMetadata, path: String, query: Stri
   -> ServerMetadata
 {
   let host = Environment.get("APP_HOSTNAME") ?? "127.0.0.1"
+  var ampersand = ""
+  if query != "" {
+    ampersand = "&"
+  }
 
   // Example url host/questions/search?page=1&q=example
   let lastPage = Int(ceil(Double(metadata.total) / Double(metadata.per)))
-  let first = "\(host)\(path)?page=1\(query)"
-  let last = "\(host)\(path)?page=\(lastPage)\(query)"
-  let current = "\(host)\(path)?page=\(metadata.page)\(query)"
-  var previous: String? = "\(host)\(path)?page=\(metadata.page - 1)\(query)"
-  var next: String? = "\(host)\(path)?page=\(metadata.page + 1)\(query)"
+  let first = "\(host)\(path)?page=1\(ampersand)\(query)"
+  let last = "\(host)\(path)?page=\(lastPage)\(ampersand)\(query)"
+  let current = "\(host)\(path)?page=\(metadata.page)\(ampersand)\(query)"
+  var previous: String? = "\(host)\(path)?page=\(metadata.page - 1)\(ampersand)\(query)"
+  var next: String? = "\(host)\(path)?page=\(metadata.page + 1)\(ampersand)\(query)"
 
   if metadata.page <= 1 {
     previous = nil
