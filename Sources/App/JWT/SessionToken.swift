03b60656f38ccf9b7c59dcfd4fe539102b37f042
kuro-vale <jsalcedo218@gmail.com>
2024-06-22 13:10:27 -0500

    Add cors and change token payload

--- Sources/App/JWT/SessionToken.swift
+++ Sources/App/JWT/SessionToken.swift
@@ -5,10 +5,12 @@ struct SessionToken: Content, Authenticatable, JWTPayload {
   // Token Data
   var expiration: ExpirationClaim
   var userId: UUID
+  var username: String
 
   // 1_296_000 are 15 days in seconds
-  init(userId: UUID, expirationTime: TimeInterval = 1_296_000) {
+  init(userId: UUID, username: String, expirationTime: TimeInterval = 1_296_000) {
     self.userId = userId
+    self.username = username
     self.expiration = ExpirationClaim(value: Date().addingTimeInterval(expirationTime))
   }
 
